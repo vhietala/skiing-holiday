f58addbaf2354a519664b7b84e666793fa1a9bbf
Mikael M <mikael_96@live.com>
2018-03-12 11:15:45 +0200

    tokennnnnnnnloveeee

--- src/providers/media/media.ts
+++ src/providers/media/media.ts
@@ -20,10 +20,6 @@ export class MediaProvider {
   uploadUrl = 'http://media.mw.metropolia.fi/wbma/uploads/';
   favouriteUrl = 'http://media.mw.metropolia.fi/wbma/favourites/';
 
-  tokenSettings = {
-    headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token')),
-  };
-
 
   constructor(public http: HttpClient) {
     console.log('Hello MediaProvider Provider');
@@ -39,11 +35,17 @@ export class MediaProvider {
   }
 
   public getNewFiles() {
-    return this.http.get(this.mediaUrl, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.get(this.mediaUrl, tokenSettings);
   }
 
   public getOneFile(id) {
-    return this.http.get<Array<string>>(this.mediaUrl + '/' + id, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.get<Array<string>>(this.mediaUrl + '/' + id, tokenSettings);
   }
 
   public register(user) {
@@ -58,7 +60,10 @@ export class MediaProvider {
   }
 
   public uploading(file) {
-    return this.http.post(this.mediaUrl, file, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.post(this.mediaUrl, file, tokenSettings);
   }
 
   public favouritesByFileId(id: number) {
@@ -74,11 +79,17 @@ export class MediaProvider {
       file_id: id,
       comment: this.newComment,
     };
-    return this.http.post(this.apiUrl + '/comments', body, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.post(this.apiUrl + '/comments', body, tokenSettings);
   }
 
   public getUserInfo(id: number) {
-    return this.http.get(this.apiUrl + '/users/' + id, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.get(this.apiUrl + '/users/' + id, tokenSettings);
   }
 
   public getByTag(tag: string) {
@@ -90,18 +101,27 @@ export class MediaProvider {
       file_id: id,
       tag: tag
     };
-    return this.http.post(this.apiUrl + '/tags', body, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.post(this.apiUrl + '/tags', body, tokenSettings);
   }
 
   public addFavourite(id: number) {
     const body = {
       file_id: id
     };
-    return this.http.post(this.favouriteUrl, body, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.post(this.favouriteUrl, body, tokenSettings);
   }
 
   public deleteFavouite(id: number) {
-    return this.http.delete(this.favouriteUrl + 'file/' + id, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.delete(this.favouriteUrl + 'file/' + id, tokenSettings);
   }
 
   public searchImages() {
@@ -109,11 +129,17 @@ export class MediaProvider {
       title: this.searchText,
       description: this.searchText
     };
-    return this.http.post(this.mediaUrl + '/search', body, this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.post(this.mediaUrl + '/search', body, tokenSettings);
   }
 
   public getUsersMedia() {
-    return this.http.get(this.mediaUrl + '/user', this.tokenSettings);
+    const tokenSettings = {
+      headers: new HttpHeaders().set('x-access-token', localStorage.getItem('token'))
+    };
+    return this.http.get(this.mediaUrl + '/user', tokenSettings);
   }
 }
 
